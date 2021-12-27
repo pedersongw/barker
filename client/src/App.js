6f95238b5ec357127b51265d7d62765449beb424
Garrett Pederson <pedersongw@gmail.com>
2021-12-27 13:59:38 -0500

    added burger menu

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Main from "./components/main";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./global";
+import { theme } from "./theme";
+import { useOnClickOutside } from "./hooks";
+import Burger from "./components/Burger/index.jsx";
+import Menu from "./components/Menu/index.jsx";
 
 function App() {
+  const [open, setOpen] = useState(false);
+
+  const node = useRef();
+  useOnClickOutside(node, () => setOpen(false));
+
   return (
-    <div className="App">
-      <Main />
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <div className="App">
+        <Main />
+        <div ref={node}>
+          <Burger open={open} setOpen={setOpen} />
+          <Menu open={open} setOpen={setOpen} />
+        </div>
+      </div>
+    </ThemeProvider>
   );
 }
 
