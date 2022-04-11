cf2abbd31bb67cb644e24fe69778b5d1481c4095
Garrett Pederson <pedersongw@gmail.com>
2022-04-11 13:28:31 -0400

    redesigned css for forumdesktopnav

--- client/src/components/ForumDesktopNav.jsx
+++ client/src/components/ForumDesktopNav.jsx
@@ -2,61 +2,127 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 class ForumDesktopNav extends React.Component {
+  state = {
+    navOpen: false,
+  };
+
+  sortByClicked = () => {
+    if (!this.state.navOpen) {
+      this.setState({ navOpen: true });
+      window.addEventListener("click", this.handleClickOutsideSortBy);
+    } else {
+      this.setState({ navOpen: false });
+      window.removeEventListener("click", this.handleClickOutsideSortBy);
+    }
+  };
+
+  handleClickOutsideSortBy = (event) => {
+    if (event.target.id === "desktop-sort-by") {
+      return;
+    } else if (event.target.className !== "desktop-nav-li") {
+      this.sortByClicked();
+    }
+  };
+
+  sortByArgument = (argument) => {
+    this.setState({ navOpen: false });
+    window.location.href = `/forum/1/${argument}`;
+  };
+
   render() {
     return (
-      <nav className="desktop-nav">
-        {this.props.user && this.props.user.isAdmin && (
-          <Link to="/admin">
-            <button>Admin</button>
-          </Link>
-        )}
+      <nav
+        className="desktop-nav"
+        id={this.state.navOpen ? "desktop-nav" : null}
+      >
+        <div className="desktop-nav-inner-wrapper">
+          {this.props.user && this.props.user.isAdmin && (
+            <div
+              className="desktop-nav-div"
+              onClick={() => console.log(this.state)}
+            >
+              Admin
+            </div>
+          )}
+          {!this.props.userLoggedIn() && (
+            <div
+              className="desktop-nav-div"
+              onClick={() => (window.location.href = "/login/create")}
+            >
+              Create New User
+            </div>
+          )}
+          {!this.props.userLoggedIn() && (
+            <div
+              className="desktop-nav-div"
+              onClick={() => (window.location.href = "/login/login")}
+            >
+              Login
+            </div>
+          )}
+          {this.props.userLoggedIn() && (
+            <div
+              className="desktop-nav-div"
+              onClick={() => this.props.openPostModal()}
+            >
+              Create Post
+            </div>
+          )}
+          {this.props.userLoggedIn() && (
+            <div
+              className="desktop-nav-div"
+              onClick={() => this.props.logOut()}
+            >
+              Logout
+            </div>
+          )}
 
-        {!this.props.userLoggedIn() && (
-          <Link to="/login/create">
-            <button>Create New User</button>
-          </Link>
-        )}
-        {!this.props.userLoggedIn() && (
-          <Link to="/login/login">
-            <button>Login</button>
-          </Link>
-        )}
-        {this.props.userLoggedIn() && (
-          <button
-            id="create-post-button"
-            onClick={() => this.props.openPostModal()}
-          >
-            Create Post
-          </button>
-        )}
-        {this.props.userLoggedIn() && (
-          <Link to="/forum/1/all">
-            <button>All Posts</button>
-          </Link>
-        )}
-        {this.props.userLoggedIn() && (
-          <Link to="/forum/1/my">
-            <button>My Posts</button>
-          </Link>
-        )}
-        {this.props.userLoggedIn() && (
-          <Link to="/forum/1/popular">
-            <button>Sort By Popular</button>
-          </Link>
-        )}
-        {this.props.userLoggedIn() && (
-          <Link to="/forum/1/new">
-            <button>Sort By New</button>
-          </Link>
-        )}
-        {this.props.userLoggedIn() && (
-          <Link to="/forum/1/old">
-            <button>Sort By Old</button>
-          </Link>
-        )}
-        {this.props.userLoggedIn() && (
-          <button onClick={() => this.props.logOut()}>Logout</button>
-        )}
+          {this.props.userLoggedIn() && (
+            <div
+              className="desktop-nav-div"
+              id="desktop-sort-by"
+              onClick={() => this.sortByClicked()}
+            >
+              Sort By
+              <ul
+                className="desktop-sort-by-menu"
+                id={this.state.navOpen ? "desktop-sort-by-menu-open" : null}
+              >
+                <li
+                  className="desktop-nav-li"
+                  onClick={() => this.sortByArgument("all")}
+                >
+                  All
+                </li>
+                <li
+                  className="desktop-nav-li"
+                  onClick={() => this.sortByArgument("popular")}
+                >
+                  Popular
+                </li>
+
+                <li
+                  className="desktop-nav-li"
+                  onClick={() => this.sortByArgument("my")}
+                >
+                  My Posts
+                </li>
+                <li
+                  className="desktop-nav-li"
+                  onClick={() => this.sortByArgument("new")}
+                >
+                  New
+                </li>
+                <li
+                  className="desktop-nav-li"
+                  onClick={() => this.sortByArgument("old")}
+                >
+                  Old
+                </li>
+              </ul>
+            </div>
+          )}
+        </div>
       </nav>
     );
   }
