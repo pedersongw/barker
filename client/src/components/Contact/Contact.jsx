1ccf339321c69b7c5eaae473af9eff2c201e15a5
Garrett Pederson <pedersongw@gmail.com>
2022-05-26 16:41:19 -0400

    testing image carousel

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -1,14 +1,29 @@
 import React from "react";
 import TopMobileNavBar from "../Navs/TopMobileNavBar";
 import styles from "./contact.module.css";
-import logo from "../../images/logo.png";
+
+import image1 from "../../FundraisingImages/goal1images/image1.jpg";
+import image2 from "../../FundraisingImages/goal1images/image2.jpg";
+import image3 from "../../FundraisingImages/goal1images/20211002_150840.jpg";
+import image4 from "../../FundraisingImages/goal1images/20211002_151239.jpg";
+import image21 from "../../FundraisingImages/goal2images/20211002_150515.jpg";
+import image22 from "../../FundraisingImages/goal2images/20211002_150601.jpg";
+import image23 from "../../FundraisingImages/goal2images/20211002_151607.jpg";
+import image31 from "../../FundraisingImages/goal3images/20211002_150715.jpg";
+import image32 from "../../FundraisingImages/goal3images/20211002_150744 (2).jpg";
 
 class Contact extends React.Component {
   state = {
     width: window.innerWidth,
-    oneClicked: 0,
-    oneClass: styles.boxOne,
+    staging: 1,
+    oneClass: styles.imgOne,
+    twoClass: styles.imgTwo,
+    threeClass: styles.imgThree,
+    fourClass: styles.imgHide,
     oneId: null,
+    twoId: null,
+    threeId: null,
+    fourId: null,
     noClick: false,
   };
 
@@ -26,65 +41,114 @@ class Contact extends React.Component {
   };
 
   leftClicked = () => {
-    const { oneClicked, noClick } = this.state;
-    if (noClick) {
+    if (this.state.noClick) {
       return;
     }
     console.log("clicked");
-    if (oneClicked === 0) {
-      this.setState({ oneId: styles.boxOneMovedLeft });
-      this.setState({ noClick: true });
-      setTimeout(() => this.setState({ noClick: false }), 1000);
-      setTimeout(() => this.setState({ oneClass: styles.boxTwo }), 1000);
-      setTimeout(() => this.setState({ oneId: null }), 1000);
-      this.setState({ oneClicked: 1 });
-    } else if (oneClicked === 1) {
-      this.setState({ oneId: styles.boxTwoMovedLeft });
-      this.setState({ noClick: true });
-      setTimeout(() => this.setState({ noClick: false }), 1000);
-      setTimeout(() => this.setState({ oneClass: styles.boxThree }), 1000);
-      setTimeout(() => this.setState({ oneId: null }), 1000);
-      this.setState({ oneClicked: 2 });
-    } else if (oneClicked === 2) {
-      this.setState({ oneId: styles.boxThreeMovedLeft });
-      this.setState({ noClick: true });
-      setTimeout(() => this.setState({ noClick: false }), 1000);
-      setTimeout(() => this.setState({ oneClass: styles.boxOne }), 1000);
-      setTimeout(() => this.setState({ oneId: null }), 1000);
-      this.setState({ oneClicked: 0 });
+    const {
+      staging,
+      oneClass,
+      twoClass,
+      threeClass,
+      fourClass,
+      oneId,
+      twoId,
+      threeId,
+      fourId,
+    } = this.state;
+    this.setState({ noClick: true });
+    setTimeout(() => {
+      this.setState({ noClick: false });
+    }, 1000);
+    if (staging === 1) {
+      this.setState({ twoId: styles.twoToOne });
+      this.setState({ oneClass: styles.imgHide });
+      this.setState({ threeId: styles.threeToTwo });
+      setTimeout(() => {
+        this.setState({ twoClass: styles.imgOne });
+      }, 1000);
+      setTimeout(() => {
+        this.setState({ twoId: null });
+      }, 1000);
+      setTimeout(() => {
+        this.setState({ threeClass: styles.imgTwo });
+      }, 1000);
+      setTimeout(() => {
+        this.setState({ threeId: null });
+      }, 1000);
+      this.setState({ staging: 4 });
+    } else if (staging === 2) {
+      this.setState({ oneId: styles.twoToOne });
+      this.setState({ fourClass: styles.imgHide });
+      this.setState({ twoId: styles.threeToTwo });
+      setTimeout(() => {
+        this.setState({ oneClass: styles.imgOne });
+      }, 1000);
+      setTimeout(() => {
+        this.setState({ oneId: null });
+      }, 1000);
+      setTimeout(() => {
+        this.setState({ twoClass: styles.imgTwo });
+      }, 1000);
+      setTimeout(() => {
+        this.setState({ twoId: null });
+      }, 1000);
+      this.setState({ staging: 1 });
     }
   };
 
   rightClicked = () => {
-    const { oneClicked, noClick } = this.state;
-    if (noClick) {
+    if (this.state.noClick) {
       return;
     }
-    if (oneClicked === 1) {
-      this.setState({ oneId: styles.boxTwoMovedRight });
+    console.log("clicked");
+    const {
+      staging,
+      oneClass,
+      twoClass,
+      threeClass,
+      fourClass,
+      oneId,
+      twoId,
+      threeId,
+      fourId,
+    } = this.state;
+    if (staging === 1) {
       this.setState({ noClick: true });
-      setTimeout(() => this.setState({ noClick: false }), 1000);
-      setTimeout(() => this.setState({ oneClass: styles.boxOne }), 1000);
-      setTimeout(() => this.setState({ oneId: null }), 1000);
-      this.setState({ oneClicked: 0 });
-    } else if (oneClicked === 2) {
-      this.setState({ oneId: styles.boxThreeMovedRight });
-      this.setState({ noClick: true });
-      setTimeout(() => this.setState({ noClick: false }), 1000);
-      setTimeout(() => this.setState({ oneClass: styles.boxTwo }), 1000);
-      setTimeout(() => this.setState({ oneId: null }), 1000);
-      this.setState({ oneClicked: 1 });
-    } else {
-      this.setState({ oneId: styles.boxOneMovedRight });
-      this.setState({ noClick: true });
-      setTimeout(() => this.setState({ noClick: false }), 1000);
-      setTimeout(() => this.setState({ oneClass: styles.boxThree }), 1000);
-      setTimeout(() => this.setState({ oneId: null }), 1000);
-      this.setState({ oneClicked: 2 });
+      setTimeout(() => {
+        this.setState({ noClick: false });
+      }, 1000);
+      this.setState({ twoId: styles.twoToThree });
+      this.setState({ threeClass: styles.imgHide });
+      this.setState({ oneId: styles.oneToTwo });
+      this.setState({ fourClass: styles.imgOne });
+      setTimeout(() => {
+        this.setState({ twoClass: styles.imgThree });
+      }, 1000);
+      setTimeout(() => {
+        this.setState({ twoId: null });
+      }, 1000);
+      setTimeout(() => {
+        this.setState({ oneClass: styles.imgTwo });
+      }, 1000);
+      setTimeout(() => {
+        this.setState({ threeId: null });
+      }, 1000);
+      this.setState({ staging: 2 });
     }
   };
 
   render() {
+    const {
+      oneClass,
+      twoClass,
+      threeClass,
+      fourClass,
+      oneId,
+      twoId,
+      threeId,
+      fourId,
+    } = this.state;
     return (
       <div className={styles.wrapper}>
         <TopMobileNavBar />
@@ -99,7 +163,20 @@ class Contact extends React.Component {
           <div id={styles.one} onClick={() => this.leftClicked()}>
             Left
           </div>
-          <div id={styles.two}></div>
+          <div id={styles.two}>
+            <div className={oneClass} id={oneId}>
+              <img alt="text" src={image1} className={styles.image} />
+            </div>
+            <div className={twoClass} id={twoId}>
+              <img alt="text" src={image2} className={styles.image} />
+            </div>
+            <div className={threeClass} id={threeId}>
+              <img alt="text" src={image3} className={styles.image} />
+            </div>
+            <div className={fourClass} id={fourId}>
+              <img alt="text" src={image4} className={styles.image} />
+            </div>
+          </div>
           <div id={styles.three} onClick={() => this.rightClicked()}>
             Right
           </div>
